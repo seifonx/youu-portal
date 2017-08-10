410561fffff990e7697ad4645ba9900c6e1488bc
Seifon <Seifon@163.com>
2017-08-10 10:18:12 +0800

    打通页面，解决404

--- src/main/java/com/youu/youu/portal/controller/ControllerTest.java
+++ src/main/java/com/youu/youu/portal/controller/ControllerTest.java
@@ -1,6 +1,16 @@
 package com.youu.youu.portal.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class ControllerTest {
+    
+    @ResponseBody
+    @RequestMapping("hello")
+    public String hello() {
+        return "hello";
+    }
 
 }
